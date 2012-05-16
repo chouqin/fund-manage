a8838c36d4811abc4182bd29eaeef9597b1ef50f
smy <smy@youremail.com>
2012-05-16 20:55:00 +0800

    datepicker

--- funds/static/js/control.js
+++ funds/static/js/control.js
@@ -1,3 +1,6 @@
 $('document').ready(function(){
     $('input, textarea, select').uniform();
+    $("#cdate").datepicker({ showOn: 'button',buttonImageOnly: true, buttonImage: '/static/images/icon_cal.png', altField: 'input#cdate', altFormat: 'yy-mm-dd'});
+    $("#edate").datepicker({ showOn: 'button', buttonImageOnly: true, buttonImage: '/static/images/icon_cal.png', altField: 'input#edate', altFormat: 'yy-mm-dd' });
+    
 });
