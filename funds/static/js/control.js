0140c98329f7783b0ecd72d3b34bd781c7a35b0b
liqi <liqiping1991@gmail.com>
2012-05-17 18:17:54 +0800

    add device and business redirect

--- funds/static/js/control.js
+++ funds/static/js/control.js
@@ -1,39 +1,13 @@
 $('document').ready(function(){
-    //var autocomleteOption = {
-        //source: ['教师1', '教师2', '俞勇', '茅旭初'],
-        //select: function(event, ui){
-            ////ui.item.option.selected = true;
-            //$(this).data('uiItem', ui.item.value);
-            //$(this).val('1234');
-            ////alert($(this).val());
-            ////self._trigger('selected', event, {
-                ////item: ui.item.option
-            ////});
-        //},
-        //change: function(event, ui) {
-            //if( !ui.item) {
-                //$(this).val('');
-                ////select.val('');
-                ////input.data('autocomplete').term = '';
-                //return false;
-            //}
-        //}
-    //};
-
     $('input, textarea, select').uniform();
     $('#cdate').datepicker();
     $('#edate').datepicker();
-    //$("#date").datepicker();
-    //$("#cdate").datepicker({ showOn: 'button',buttonImageOnly: true, buttonImage: '/static/images/icon_cal.png', altField: 'input#cdate', altFormat: 'yy-mm-dd'});
-    //$("#edate").datepicker({ showOn: 'button', buttonImageOnly: true, buttonImage: '/static/images/icon_cal.png', altField: 'input#edate', altFormat: 'yy-mm-dd' });
     $("#cdate").datepicker('option', $.datepicker.regional['zh-CN']);
     $("#cdate").datepicker('option', 'dateFormat','yy-mm-dd');//set dateFormat
-    //$( "#cdate" ).datepicker( "option", "autoSize", true );
     $( "#cdate" ).datepicker( "option", "changeMonth", true );
     $( "#cdate" ).datepicker( "option", "changeYear", true );
     $("#edate").datepicker('option', $.datepicker.regional['zh-CN']);
     $("#edate").datepicker('option', 'dateFormat','yy-mm-dd');//set dateFormat
-    //$( "#cdate" ).datepicker( "option", "autoSize", true );
     $( "#edate" ).datepicker( "option", "changeMonth", true );
     $( "#edate" ).datepicker( "option", "changeYear", true );
     $('tr:odd').addClass('alt');
@@ -86,5 +60,20 @@ $('document').ready(function(){
         //$(this).before();
         return false;
     });
-    //$("#datepicker").datepicker({clickInput:true});
+    $('button').button();
+    $('button.project-add-device').click(function(){
+        $('#submit_type').val('add_device');
+        $('form').submit();
+        return false;
+    });
+    $('button.project-add-business').click(function(){
+        $('#submit_type').val('add_business');
+        $('form').submit();
+        return false;
+    });
+    $('button.project-add').click(function(){
+        $('#submit_type').val('save');
+        $('form').submit();
+        return false;
+    });
 });
